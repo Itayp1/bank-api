264191ddb88259706f94ad41be5d744ea17ce8bb
itayp1 <peretz.itay@gmail.com>
2020-11-18 01:30:04 +0200

    update mongo connection

--- server.config.js
+++ server.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 3001,
-  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost/bank-api',
+  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING || 'mongodb+srv://bank:bank1234@cluster0.ycnml.mongodb.net/bankapi?retryWrites=true&w=majority',
 };
